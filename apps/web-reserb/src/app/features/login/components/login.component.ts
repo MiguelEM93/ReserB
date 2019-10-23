fae5e6d4dec8965fdfc3f21ce1f056d809f3e22f
Miguel <miguel.em@tcs.com>
2019-10-23 00:35:00 -0500

    changes in login

--- apps/web-reserb/src/app/features/login/components/login.component.ts
+++ apps/web-reserb/src/app/features/login/components/login.component.ts
@@ -1,5 +1,6 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { BaseComponent } from '@reserb-app/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'reserb-login',
@@ -7,10 +8,23 @@ import { BaseComponent } from '@reserb-app/core';
   styleUrls: ['login.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
-export class LoginComponent extends BaseComponent {
+export class LoginComponent extends BaseComponent implements OnInit {
+
+  formLogin : FormGroup;
 
   constructor(){
     super();
+  }
+
+  ngOnInit(): void {
+    this.initForm();
+  }
+
+  initForm(): void {
+    this.formLogin = new FormGroup({
+      username: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required])
+    });
   }
 
 }
